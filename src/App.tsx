b0f8a399ea8f907522dd605be0306d8fad1f968d
Sai <saikumar7557@gmail.com>
2023-11-09 15:05:29 +0530

    Config - Add eslint & prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react';
 
 import './App.css';
 
-const App = () => {
-    return (
-        <div className="maincontent">This is App component</div>
-    );
-}
+const App = (): JSX.Element => {
+  return <div className="maincontent">This is App component</div>;
+};
 
 export default App;
